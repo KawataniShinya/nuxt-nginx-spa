bc374523ecfe46ab928207236c4b602f42e2dfb9
KawataniShinya <peritrochoidonsilverfox@gmail.com>
2023-02-19 12:12:52 +0900

    add nuxt dynamic routing

---
+++ pages/products/_id/show.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    商品詳細<br>
+    {{ $route.params.id }}
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/products/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    商品一覧<br>
+    <table>
+      <tr>
+        <th>商品番号</th>
+        <th>商品名</th>
+        <th>詳細</th>
+      </tr>
+      <tr>
+        <td>1</td>
+        <td>商品1</td>
+        <td><NuxtLink to="/products/1/show">詳細</NuxtLink></td>
+      </tr>
+      <tr>
+        <td>2</td>
+        <td>商品2</td>
+        <td><NuxtLink to="/products/2/show">詳細</NuxtLink></td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
